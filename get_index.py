d97bfb1de0135896c59b0f09c899bcdc7da3a6b7
joaovae <joao.victor.a.e@usp.br>
2020-08-19 10:55:09 -0300

    add_turkey

--- get_index.py
+++ get_index.py
@@ -5,15 +5,14 @@ Created on Tue Aug 18 20:48:54 2020
 @author: JOAO VICTOR
 """
 
-import yfinance as yf
 import pandas_datareader.data as web
 import pandas as pd
-import warnings
-import numpy as np
 
-cryptocurrencies = ['BTC-USD', 'ETH-USD', 'XRP-USD']
-dados_yahoo = web.get_data_yahoo(cryptocurrencies, start='2013-01-01', end='2020-01-01')['Adj Close']
+cryptocurrencies = ['BTC-USD']
+dados_crypto = web.get_data_yahoo(cryptocurrencies, start='2013-01-01', end='2020-01-01')['Adj Close']
 
+indexs = ['IMOEX.ME', '^BVSP', '^MXX', '^BSESN', '^JKSE', 'XU100.IS']
+dados_index = web.get_data_yahoo(indexs, start='2013-01-01', end='2020-01-01')['Adj Close']
 
-indexs = ['IMOEX.ME', '^BVSP', '^MXX', '^BSESN', '^JKSE']
-dados_yahoo = web.get_data_yahoo(indexs, start='2013-01-01', end='2020-01-01')['Adj Close']
+dados_index.to_csv('dados_index.csv')
+dados_crypto.to_csv('dados_crypto.csv')
