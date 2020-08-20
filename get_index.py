86c595b3c11a5fb8522011c5dc45057b7051ceda
joaovae <joao.victor.a.e@usp.br>
2020-08-20 18:52:35 -0300

    add_benchs

--- get_index.py
+++ get_index.py
@@ -11,12 +11,17 @@ import pandas as pd
 cryptocurrencies = ['BTC-USD']
 dados_crypto = web.get_data_yahoo(cryptocurrencies, start='2013-01-01', end='2020-01-01')['Adj Close']
 
-indexs = ['IMOEX.ME', '^BVSP', '^MXX', '^BSESN', '^JKSE', 'XU100.IS']
-dados_index = web.get_data_yahoo(indexs, start='2013-01-01', end='2020-01-01')['Adj Close']
+indexs = ['IMOEX.ME', '^BVSP', '^MXX', '^BSESN', '^JKSE', 'XU100.IS', '000001.SS', '^HSI']
+dados_index = web.get_data_yahoo(indexs, start='2013-01-01', end='2020-08-24')['Adj Close']
 
 
 dados_crypto.to_csv('dados_crypto.csv')
 
 for i in indexs:
-    dados_index = web.get_data_yahoo(i, start='2013-01-01', end='2020-01-01')['Adj Close']
+    dados_index = web.get_data_yahoo(i, start='2013-01-01', end='2020-08-24')['Adj Close']
     dados_index.to_csv('{}.csv'.format(i))
+
+benchs = ['^GSPC', '^DJI', '^IXIC']
+for i in benchs:
+    dados_bench = web.get_data_yahoo(i, start='2013-01-01', end='2020-08-24')['Adj Close']
+    dados_index.to_csv('bench_{}.csv'.format(i))
