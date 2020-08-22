a6c7851b07a614c83ebd0e9dec14d6c6a9940c3c
joaovae <joao.victor.a.e@usp.br>
2020-08-22 15:25:18 -0300

    mudanca_cor

--- analise_china.R
+++ analise_china.R
@@ -5,13 +5,16 @@ shangai <- read.csv(file = "dado_shangai.csv", fill = FALSE)
 hong_kong <- read.csv(file = "dado_hongkong.csv", fill = FALSE)
 crypto <- read.csv(file = "dados_crypto.csv", fill = FALSE)
 
-trans_ts_and_get_returns <- function(data_prices){
-  
+trans_ts <- function(data_prices){
 #transforma em time series
   stock_data <- xts(data_prices[,-1], order.by=as.Date(data_prices[,1], "%Y-%m-%d"))
-  
+  return(stock_data)
+} 
+
+get_return <- function(data) { 
+  data <- trans_ts(data)
 #pega o a variação percentual e preenche o primeiro valor com 1
-  pct_change_data <- (stock_data/lag(stock_data)) - 1
+  pct_change_data <- (data/lag(data)) - 1
   
   pct_change_data <- na.fill(pct_change_data, fill = 0)
   
@@ -20,7 +23,16 @@ trans_ts_and_get_returns <- function(data_prices){
   return(return_data)
 }
 
-return_shangai <- trans_ts_and_get_returns(shangai)
-return_hongkong <- trans_ts_and_get_returns(hong_kong)
-return_crypto <- trans_ts_and_get_returns(crypto)
+return_shangai <- get_return(shangai)
+return_hongkong <- get_return(hong_kong)
+return_crypto <- get_return(crypto)
 
+dado_shangai <- trans_ts(shangai)
+dado_hongkong <- trans_ts(hong_kong)
+dado_crypto <- trans_ts(crypto)
+
+merg_shangai_crypto <- merge(dado_shangai, dado_crypto, join="inner")
+merg_hongkong_crypto <- merge(dado_hongkong, dado_crypto, join="inner")
+
+cor_hongkong <- cor(merg_hongkong_crypto)
+cor_shangai <- cor(merg_shangai_crypto)
